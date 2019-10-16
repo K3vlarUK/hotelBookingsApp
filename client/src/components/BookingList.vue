4f5c32cba55a345b5b4ea0c35777137917ed3dfb
Kevin McDermott <kmcdermott31@gmail.com>
2019-10-16 12:50:04 +0100

    added list

---
+++ client/src/components/BookingList.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div id="bookingsList">
+    <ul>
+      
+    </ul>
+  </div>
+</template>
+
+<script>
+import {eventBus} from '../main.js'
+import BookingService from '../services/BookingService';
+
+export default {
+  name: 'booking-list',
+  props: ['bookings'],
+  methods: {
+    handleDelete(id) {
+      BookingService.deleteBooking(id)
+      .then(response => eventBus.$emit('booking-deleted', id));
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
